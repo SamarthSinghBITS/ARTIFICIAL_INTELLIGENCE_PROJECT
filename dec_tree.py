7d97926b74ed97314285cd5efa2253e3a75cb6ad
Samarth Singh <43131572+SamarthSinghBITS@users.noreply.github.com>
2019-11-04 22:25:34 +0530

    Update dec_tree.py

--- dec_tree.py
+++ dec_tree.py
@@ -9,6 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import export_graphviz
 from sklearn.neural_network import MLPClassifier
 from sklearn import svm
+import tkinter as tk
 
 # Load data file
 data = pd.read_csv('bank.csv')
@@ -78,8 +79,8 @@ bank_with_dummies = pd.get_dummies(data=bank_data, columns=['job', 'marital', 'e
                                    prefix=['job', 'marital', 'education'])
 print(bank_with_dummies)
 
-for col in bank_with_dummies.columns:
-    print(col)
+# for col in bank_with_dummies.columns:
+#     print(col)
 # make a copy
 bankcl = bank_with_dummies
 
@@ -95,6 +96,8 @@ data_train, data_test, label_train, label_test = train_test_split(data_drop_depo
 # print(data_test)
 # print(label_train)
 # print(label_test)
+# for col in data_test.columns:
+#     print(col)
 
 
 # Function to make predictions
@@ -166,6 +169,7 @@ def train_using_knn(x_train, y_train):
     knn1.fit(x_train, y_train)
     return knn1
 
+
 # Training Using KNN Algorithm
 knn = train_using_knn(data_train, label_train)
 
@@ -178,10 +182,12 @@ cal_accuracy(label_test, predict_knn)
 
 # MLP Algorithm
 def train_using_mlp(x_train, y_train):
-    mlp1 = MLPClassifier(solver='adam', activation='logistic', alpha=1e-5, hidden_layer_sizes=(7), random_state=1, epsilon=1e-9)
+    mlp1 = MLPClassifier(solver='adam', activation='logistic', alpha=1e-5, hidden_layer_sizes=(7),
+                         random_state=1, epsilon=1e-9)
     # Performing training
     mlp1.fit(x_train, y_train)
     return mlp1
+
 
 # Training Using MLP Algorithm
 mlp = train_using_mlp(data_train, label_train)
@@ -200,6 +206,7 @@ def train_using_svm(x_train, y_train):
     svclf1.fit(x_train, y_train)
     return svclf1
 
+
 # Training Using Linear SVC Algorithm
 svclf = train_using_svm(data_train, label_train)
 
@@ -208,3 +215,167 @@ predict_svm = svclf.predict(data_test)
 
 # Calculate Precision, Recall, F-measure and Accuracy of the model
 cal_accuracy(label_test, predict_svm)
+
+main_input = []
+
+
+def decision():
+    try:
+        main_input.append(int(age.get("1.0", 'end-1c')))
+        main_input.append(job_var.get())
+        main_input.append(mar_var.get())
+        main_input.append(edu_var.get())
+        main_input.append(int(balance.get("1.0", 'end-1c')))
+        main_input.append(int(duration.get("1.0", 'end-1c')))
+        main_input.append(int(campaign.get("1.0", 'end-1c')))
+        main_input.append(option_var_default.get())
+        main_input.append(option_var_housing.get())
+        main_input.append(option_var_loan.get())
+        main_input.append(int(recent_pdays.get("1.0", 'end-1c')))
+        if main_input[7] == 'Yes':
+            main_input[7] = 1
+        else:
+            main_input[7] = 0
+
+        if main_input[8] == 'Yes':
+            main_input[8] = 1
+        else:
+            main_input[8] = 0
+
+        if main_input[9] == 'Yes':
+            main_input[9] = 1
+        else:
+            main_input[9] = 0
+
+        if main_input[0] < 0 or main_input[5] < 0 or main_input[6] < 0:
+            raise ValueError
+
+        print(main_input[0], main_input[1])
+        bank = pd.DataFrame([main_input])
+        bank.columns = ['age', 'job', 'marital', 'education', 'balance', 'duration', 'campaign', 'default_cat',
+                        'housing_cat', 'loan_cat', 'recent_pdays']
+        print(bank)
+        bank["job_blue-collar"] = bank['job'].map({'Blue Collar': 1, 'Entrepreneur': 0, 'Self Employed': 0,
+                                                   'Pink Collar': 0, 'Technician': 0, 'White Collar': 0, 'Others': 0})
+        bank["job_entrepreneur"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 1, 'Self Employed': 0,
+                                                    'Pink Collar': 0, 'Technician': 0, 'White Collar': 0, 'Others': 0})
+        bank["job_other"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 0, 'Self Employed': 0,
+                                             'Pink Collar': 0, 'Technician': 0, 'White Collar': 0, 'Others': 1})
+        bank["job_pink-collar"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 0, 'Self Employed': 0,
+                                                   'Pink Collar': 1, 'Technician': 0, 'White Collar': 0, 'Others': 0})
+        bank["job_self-employed"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 0, 'Self Employed': 1,
+                                                     'Pink Collar': 0, 'Technician': 0, 'White Collar': 0, 'Others': 0})
+        bank["job_technician"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 0, 'Self Employed': 0,
+                                                  'Pink Collar': 0, 'Technician': 1, 'White Collar': 0, 'Others': 0})
+        bank["job_white-collar"] = bank['job'].map({'Blue Collar': 0, 'Entrepreneur': 0, 'Self Employed': 0,
+                                                    'Pink Collar': 0, 'Technician': 0, 'White Collar': 1, 'Others': 0})
+        bank.drop('job', axis=1, inplace=True)
+        bank["marital_divorced"] = bank['marital'].map({'Divorced': 1, 'Married': 0, 'Single': 0})
+        bank["marital_married"] = bank['marital'].map({'Divorced': 0, 'Married': 1, 'Single': 0})
+        bank["marital_single"] = bank['marital'].map({'Divorced': 0, 'Married': 0, 'Single': 1})
+        bank.drop('marital', axis=1, inplace=True)
+        bank["education_primary"] = bank['education'].map({'Primary': 1, 'Secondary': 0, 'Tertiary': 0, 'Unknown': 0})
+        bank["education_secondary"] = bank['education'].map({'Primary': 0, 'Secondary': 1, 'Tertiary': 0, 'Unknown': 0})
+        bank["education_tertiary"] = bank['education'].map({'Primary': 0, 'Secondary': 0, 'Tertiary': 1, 'Unknown': 0})
+        bank["education_unknown"] = bank['education'].map({'Primary': 0, 'Secondary': 0, 'Tertiary': 0, 'Unknown': 1})
+        bank.drop('education', axis=1, inplace=True)
+        print(bank)
+        for col in bank.columns:
+            print(col)
+        predict_knn_f = prediction(bank, knn)
+        predict_cart_f = prediction(bank, gini)
+        predict_id_f = prediction(bank, Info_gain)
+        predict_mlp_f = prediction(bank, mlp)
+        predict_svm_f = prediction(bank, svclf)
+        sum_f = predict_knn_f + predict_cart_f + predict_id_f + predict_mlp_f + predict_svm_f
+        if sum_f < 3:
+            predict = 0
+        else:
+            predict = 1
+        print(predict)
+
+    except ValueError:
+        print("Invalid input")
+
+
+master = tk.Tk()
+# master.geometry("1366x768")
+master.title("Enter the client's details:")
+tk.Label(master, text='Age').grid(row=0)
+
+Jobs = ['Blue Collar', 'Entrepreneur', 'Pink Collar', 'Self Employed', 'Technician', 'White Collar', 'Others']
+job_var = tk.StringVar(master)
+job_var.set(Jobs[0])
+
+Marital = ['Divorced', 'Married', 'Single']
+mar_var = tk.StringVar(master)
+mar_var.set(Marital[0])
+
+Education = ['Primary', 'Secondary', 'Tertiary', 'Unknown']
+edu_var = tk.StringVar(master)
+edu_var.set(Education[0])
+
+Yes_No_default = ['Yes', 'No']
+option_var_default = tk.StringVar(master)
+option_var_default.set(Yes_No_default[0])
+
+Yes_No_housing = ['Yes', 'No']
+option_var_housing = tk.StringVar(master)
+option_var_housing.set(Yes_No_housing[0])
+
+Yes_No_loan = ['Yes', 'No']
+option_var_loan = tk.StringVar(master)
+option_var_loan.set(Yes_No_loan[0])
+
+tk.Label(master, text='Job').grid(row=1)
+tk.Label(master, text='Marital Status').grid(row=2)
+tk.Label(master, text='Education').grid(row=3)
+tk.Label(master, text='Balance').grid(row=4)
+tk.Label(master, text='Duration').grid(row=5)
+tk.Label(master, text='Campaign').grid(row=6)
+tk.Label(master, text='Default').grid(row=7)
+tk.Label(master, text='Housing').grid(row=8)
+tk.Label(master, text='Loan').grid(row=9)
+tk.Label(master, text='Recent_Pdays').grid(row=10)
+
+age = tk.Text(master, height=1, width=10)
+
+# job = tk.Entry(master)
+job = tk.OptionMenu(master, job_var, *Jobs)
+
+# marital = tk.Entry(master)
+marital = tk.OptionMenu(master, mar_var, *Marital)
+
+# education = tk.Entry(master)
+education = tk.OptionMenu(master, edu_var, *Education)
+
+balance = tk.Text(master, height=1, width=10)
+duration = tk.Text(master, height=1, width=10)
+campaign = tk.Text(master, height=1, width=10)
+
+# default = tk.Entry(master)
+default = tk.OptionMenu(master, option_var_default, *Yes_No_default)
+
+# housing = tk.Entry(master)
+housing = tk.OptionMenu(master, option_var_housing, *Yes_No_housing)
+
+# loan = tk.Entry(master)
+loan = tk.OptionMenu(master, option_var_loan, *Yes_No_loan)
+
+recent_pdays = tk.Text(master, height=1, width=10)
+
+age.grid(row=0, column=1)
+job.grid(row=1, column=1)
+marital.grid(row=2, column=1)
+education.grid(row=3, column=1)
+balance.grid(row=4, column=1)
+duration.grid(row=5, column=1)
+campaign.grid(row=6, column=1)
+default.grid(row=7, column=1)
+housing.grid(row=8, column=1)
+loan.grid(row=9, column=1)
+recent_pdays.grid(row=10, column=1)
+submit = tk.Button(master, text='Submit', width=25, command=decision)
+submit.grid(row=11, column=1)
+
+tk.mainloop()
